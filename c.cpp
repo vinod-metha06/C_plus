23e0d5562b397acd3f376d1668e286fb8eef2945
vinod-metha06 <methavinod06@gmail.com>
2021-01-09 22:42:52 +0530

    commit

---
+++ c.cpp
@@ -0,0 +1,27 @@
+c#include<iostream>
+using namespace std;
+
+#include <stdlib.h> //For system("clear") function  /Alternative of conio.h
+#include <curses.h>
+
+int main()
+{
+int a[100],n,i;
+system("clear");
+cout<<"Enter the size of array"<<endl;
+cin>>n;
+cout<<"Enter the elements of array"<<endl;
+for(i=0;i<n;i++)
+{
+cin>>a[i];
+}
+cout<<"Element in the array"<<endl;
+for(i=0;i<n;i++)
+{
+cout<<a[i];
+cout<< endl;
+}
+cout<< endl;
+
+getch();
+}
